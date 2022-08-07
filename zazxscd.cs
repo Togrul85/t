2841fe9a1c3ecd5d2e42c5edb125018b145eafd2
unknown <toghrul@code.edu.az>
2022-08-07 22:57:35 +0400

    .

---
+++ zazxscd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]  sentence = {' Salam Azerbaycan' };
+            Console.WriteLine();
+           
+
+
+
+
+
+        }
+        static void VowelLetter(int[] arr,int count,int vowel,int consonant)
+        {
+            count = 0;
+            if (arr=' a,ı,e,ə,o,u,i,ö,ü')
+            {
+                count++;
+                {
+                    return (vowel);
+                }
+                
+            }
+            
+
+
+            {
+
+            }
+
+        }
+    }
+}
